a018317b36c4a19b8179bfab20d5b0e422956d2e
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-04-29 13:35:37 +0530

    Add files via upload

---
+++ operatortask3.py
@@ -0,0 +1,102 @@
+##area of rectangle
+
+l=int(input("enter the lenth value"))
+b=int(input("enter the lenth value"))
+area=l*b
+print("Area of the rectangle is ",area)
+
+##area of square
+
+square=int(input("enter the square value"))
+area=area*square**2
+print("Area of the square value ",area)
+
+##simple intrest
+
+p=int(input("enter the pricipal ammount"))
+n=int(input("enter the number years"))
+r=int(input("enter the rate of intrest"))
+simpleintrest=p*n*r/100
+print("area of the simpleintrest value",simpleintrest)
+
+##area of triangle
+
+b=int(input("enter the triangle value"))
+h=int(input("enter the triangle value"))
+area=1/2*b*h
+print("area of the triangle value",area)
+
+
+##syntax;(input create)
+
+a=int(input("enter the number of value a"))
+b=float(input("enter the value of decimal value b"))
+c=str(input("enter the string value of c"))
+d=(input("enter the value of d"))
+print(a,b,c,d)
+
+#Addition program
+
+num1=int(input("enter the number of value of number1"))
+num2=int(input("enter the number of value of number2"))
+add=num1+num2
+print("Addition=",add)
+
+##subraction
+
+num1=int(input("enter the number of value of number1"))
+num2=int(input("enter the number of value of number2"))
+sub=num1-num2
+print("subraction=",sub)
+
+##multiplication
+
+num1=int(input("enter the number of value of number1"))
+num2=int(input("enter the number of value of number2"))
+mul=num1*num2
+print("multiplication=",mul)
+
+##division
+
+num1=int(input("enter the number of value of number1"))
+num2=int(input("enter the number of value of number2"))
+div=num1*num2
+print("division=",div)
+
+##moduls
+
+num1=int(input("enter the number of value of number1"))
+num2=int(input("enter the number of value of number2"))
+mol=num1%num2
+print("moduls=",mol)
+
+
+##floordivision
+
+num1=int(input("enter the number of value of number1"))
+num2=int(input("enter the number of value of number2"))
+flo=num1//num2
+print("floordivision=",flo)
+
+
+
+##Relational Operator
+
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+
+##logical operator
+
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+print((a>b)and(a<c))
+print((a>b)or(c<a))
+print(not(a>b))
+
