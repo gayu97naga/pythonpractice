1be161d9a66560d4740e92509efedbcfad743780
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-04-29 13:36:06 +0530

    Add files via upload
    trainer teaching

---
+++ operators.py
@@ -0,0 +1,49 @@
+#user input
+##a=10
+##b=10
+##syntax:
+##    varibale_name=datatype(input("statement"))
+##a=int(input("enter the value of a"))
+##b=float(input("enter the value of decimal value"))
+##c=str(input("enter the value of string"))
+##d=input("enter the value of d")
+##print(a,b,c,d)
+
+
+###addition program
+##num1=int(input("enter the value of number1"))
+##num2=int(input("enter the value of number2"))
+##add=num1+num2
+##print("Addition =",add)
+##div=num1/num2
+##print("division =",div)
+##mod=num1%num2
+##print("modulus =",mod)
+##floor=num1//num2
+##print("floor division =",floor)
+##
+###area of circle
+##pi=3.14
+##radius=int(input("enter the radius"))
+##area=pi*radius**2
+##print("Area of the circle is ",area)
+
+
+###Relational Operator
+##a=int(input("enter the value of a"))
+##b=int(input("enter the value of b"))
+##print(a>b)
+##print(a<b)
+##print(a>=b)
+##print(a<=b)
+##print(a==b)
+##print(a!=b)
+
+
+#logical operator
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+print((a>b)and(a<c))
+print((c>a)or (a<b))
+print(not(a>b))
