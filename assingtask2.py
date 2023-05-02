a565a30c269682aad86cf75dcbeac8eeb9bd1a1b
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-05-02 13:46:34 +0530

    Add files via upload

---
+++ assingtask2.py
@@ -0,0 +1,91 @@
+#assingment operator
+
+
+num=0.7
+num1=6
+num2=-4
+num-=-5
+num**=num1
+num-=7
+num1+=8
+num1-=0
+num1+=num2
+num2-=num
+print(num)
+print(num1)
+print(num2)
+
+
+#inmutable datatype
+
+
+int1=10
+int2=20
+print(id(int1))
+print(id(int2))
+print(int1 is int2)
+print(int1 is not int2)
+
+
+##int1=10
+##int2=10
+##print(id(int1))
+##print(id(int2))
+##print(int1 is int2)
+##print(int1 is not int2)
+
+
+
+#float
+
+
+float1=2.5
+float2=3.5
+print(id(float1))
+print(id(float2))
+print(float1 is float2)
+print(float1 is not float2)
+
+
+#string
+
+
+string=input("enter the string name")
+userinput=input("enter the character your are the check")
+print(userinput in string)
+print(userinput not in string)
+
+
+string="gayathri"
+print("g" in string)
+print("d" in string)
+print("f" in  string)
+print("f" not in string)
+
+#bitwise operator
+
+a=5
+b=9
+print(a&b)
+print(a|b)
+print(a^b)
+
+
+a=7
+b=8
+print(a&b)
+print(a|b)
+print(a^b)
+
+
+a=17
+b=10
+print(a&b)
+print(a|b)
+print(a^b)
+
+a=8
+b=13
+print(a&b)
+print(a|b)
+print(a^b)
