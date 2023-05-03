a84b0c50b4f7e4baeb31fc66f5b97585e7467268
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-05-03 13:44:04 +0530

    Add files via upload

---
+++ practiceswork.py
@@ -0,0 +1,90 @@
+#bitwise operator
+
+a=12
+b=15
+print(a&b)
+print(a|b)
+print(a^b)
+
+#assignment oper
+
+
+num=5
+num1=-8
+num2=5
+num=0.4
+num2-=-8
+num1-=7
+num2-=5
+num2+=3
+print(num)
+print(num1)
+print(num2)
+
+#arithmetic opr
+#area of rectangle
+
+l=int(input("enter the rectangle value"))
+b=int(input("enter the rectangle value"))
+area=l*b
+print("enter the rectangle value",area)
+
+
+#relational opr
+
+a=int(input("enter the value a"))
+b=int(input("enter the value b"))
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+
+#bitwise opr
+
+a=5
+b=10
+print(a&b)
+print(a|b)
+print(a^b)
+
+
+#mutable datatype
+
+a=[1,2,3,4]
+b=[1,2,3,4]
+c=a
+print((id),c)
+
+
+#bitwise opr
+#positive value(~a)
+a=9
+print(~a)
+
+#nagative value
+a=-11
+print(~a)
+
+#leftshift value(<<)
+#positive
+a=9
+print(a<<2)
+#nagative
+a=-11
+print(a<<2)
+
+
+
+#rightshift value(>>)
+#positive
+a=9
+print(a>>2)
+
+#nagative
+a=-11
+print(a<<2)
+
+
