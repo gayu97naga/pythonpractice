bf2634347db75451a61880d3b6cab7a00325349a
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-05-04 12:27:28 +0530

    Add files via upload

---
+++ bitwiseopr.py
@@ -0,0 +1,55 @@
+#bitwise operator
+#Not(~)
+#positive value
+a=6
+print(~a)
+
+a=9
+print(~a)
+
+#Negative value
+a=-5
+print(~a)
+
+a=-11
+print(~a)
+
+
+#left shift(<<)
+#positive value
+a=5
+print(a<<2)
+
+a=9
+print(a<<2)
+
+#Negative value
+a=-5
+print(a<<2)
+
+a=-11
+print(a<<2)
+
+#right shift(>>)
+#positive value
+a=5
+print(a>>2)
+
+a=10
+print(a>>2)
+
+a=9
+print(a>>2)
+
+#negative value
+a=-5
+print(a>>2)
+
+a=-15
+print(a>>2)
+
+a=-11
+print(a>>2)
+
+
+
