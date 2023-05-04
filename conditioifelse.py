ecf367d3f6d97601b227e421ede1b40d00b0e343
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-05-04 13:41:03 +0530

    Add files via upload

---
+++ conditioifelse.py
@@ -0,0 +1,16 @@
+#conditionif,else
+
+#Task:
+1.Find the age weather the senior citizen (or)not:
+
+age=int(input("enter the age"))
+if(age>=60):
+    print("you are the senior citizen")
+    
+else:
+    print("not eligible in senior citizen")
+
+
+
+
+
