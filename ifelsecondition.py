9c5c6e3c441266b16d325ab3145a025b4bf2c38a
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-05-05 12:35:17 +0530

    Add files via upload

---
+++ ifelsecondition.py
@@ -0,0 +1,38 @@
+#1.find the age whether the person is senior citizen (or)not:
+
+##age=int(input("enter the age"))
+##if(age>=60):
+##    print("you are the senior citizen")
+##
+##else:
+##    print("this person is not senior citizen")
+
+
+#2.the given number is odd(or)even:
+
+##a=int(input("enter the value of a"))
+##if(a%6==0):
+##    print("a is even")
+##
+##else:
+##    print("a is odd")
+
+
+#3.the given number is positive(or)negative:
+
+##a=int(input("enter the value a"))
+##if(a<=10) and (a>=5):
+##    print("a is positive number")
+##
+##else:
+##    print("a is negative number")
+
+#4.the given number is divisible by 3(0r)not:
+
+a=int(input("enter the value of a"))
+if(a%3==0) and (a%10==0):
+    print("3 and 5 is a divisible")
+
+else:
+    print("10 is not divisible")
+    
