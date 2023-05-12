ac8a5b0e8bb172f24b9e034297c8788a0c6f4cdd
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-05-12 13:49:07 +0530

    Add files via upload

---
+++ elifstatement.py
@@ -0,0 +1,79 @@
+#1.student percentage:
+
+##marks=int(input("enter your marks"))
+##if(marks>=90):
+##    print("your grade is A",marks)
+##elif(marks>=80):
+##    print("your grade is B",marks)
+##elif(marks>=70):
+##    print("your grade is C",marks)
+##elif(marks>=60):
+##    print("your grade is D",marks)
+##else:
+##    print("your grade is E",marks)
+
+#2.no of days:
+
+##day=int(input("enter the day"))
+##if(day==1):
+##    print("sundsy",day)
+##elif(day==2):
+##    print("monday",day)
+##elif(day==3):
+##    print("tuesday",day)
+##elif(day==4):
+##    print("wednesday",day)
+##elif(day==5):
+##    print("thursday",day)
+##elif(day==6):
+##    print("friday",day)
+##elif(day==7):
+##    print("saturday",day)
+##else:
+##    print("day is not defined")
+            
+
+
+#3.three types of triangles:
+
+##a=int(input("enter the value of a"))
+##b=int(input("enter the value of b"))
+##c=int(input("enter the value of c"))
+##if(a==b==c):
+##    print("equilateral triangle")
+##elif(a==b)or(b==c)or(c==a):
+##    print("isoscales triangles")
+##else:
+##    print("scale triangles")
+
+
+
+#4.current bill:
+
+##unit=int(input("enter the units"))
+##if(unit<=100):
+##    print("there is no charge")
+##elif(unit<=200):
+##    x=((unit-100)*5)
+##    print("next 100 units price",x)
+##elif(unit>200):
+##    y=((unit-200)*10)+500
+##    print("after 200 units",y)
+##else:
+##    print("there is no units")
+
+
+###5.road tax:
+##
+##cost=int(input("enter the bike price"))
+##if(cost>100000):
+##    tax=cost*15/100
+##    print("road tax",tax)
+##elif(cost>50000)and(cost<=100000):
+##    tax=cost*10/100
+##    print("road tax",tax)
+##else:
+##    tax=cost*5/100
+##    print("road tax",tax)
+
+    
