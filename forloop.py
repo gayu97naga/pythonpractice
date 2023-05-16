5605152dd1c78d3ac03f70107f49fcba3722a123
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-05-16 13:58:15 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,25 @@
+#1.even numbers:
+
+##stop=int(input("enter the stop value"))
+##for i in range(1,stop+1):
+##    if(i%2==0):
+##        print(i)
+
+
+#2.odd number:
+
+##stop=int(input("enter the stop value"))
+##for i in range(1,stop+1):
+##    if(i%2!=0):
+##        print(i)
+
+#3. 3 divisible :
+
+stop=int(input("enter the stop value"))
+for i in range(3):
+    print(i)
+if(i%3==0):
+    print("good")
+
+        
+                
