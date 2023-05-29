1f7cc9779ee441f7a66bc305f9f6364b182e3dc9
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-05-29 13:31:43 +0530

    Add files via upload

---
+++ nessted while loop.py
@@ -0,0 +1,29 @@
+#nested while loop:
+
+##i=1
+##while(i<=3):
+##    print("ocean",end=" ")
+##    j=1
+##    while(j<=1):
+##        print("academy",end=" ")
+##        j=j+1
+##    print()
+##    i+=1
+
+
+# * 
+# * * 
+# * * * 
+##i=0
+##while(i<=3):
+##    print("*",end=" ")
+##    j=1
+##    while(j<=i):
+##        print("*",end=" ")
+##        j=j+1
+##    print()
+##    i+=1
+
+
+
+  
