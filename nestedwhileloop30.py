7f9dc41925cbde22bc6dd024837f61f175e28cef
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-05-30 12:31:31 +0530

    Add files via upload

---
+++ nestedwhileloop30.py
@@ -0,0 +1,60 @@
+##rows=int(input("enter the rows"))
+##i=0
+##while(i<=rows):
+##    print("*",end=" ")
+##    j=1
+##    while(j<=i):
+##        print("*",end=" ")
+##        j=j+1
+##    print()
+##    i+=1
+
+
+##rows=int(input("enter the rows"))
+##i=1
+##while(i<=rows):
+##    j=rows
+##    while(j>=i):
+##        print("*",end=" ")
+##        j=j-1
+##    print()
+##    i+=1
+
+
+
+##rows=int(input("enter the rows"))
+##i=0
+##while(i<=rows):
+##    print("*",end=" ")
+##    j=1
+##    while(j<=i):
+##        print("*",end=" ")
+##        j=j+1
+##    print()
+##    i+=1
+##
+##i=1
+##while(i<=rows):
+##    j=rows
+##    while(j>=i):
+##        print("*",end=" ")
+##        j=j-1
+##    print()
+##    i+=1
+
+
+
+##rows=int(input("enter the rows"))
+i=1
+while(i<=5):
+    j=1
+    while(j<=i):
+        print(j,end=" ")
+        j=j+1
+    print()
+    i=i+1
+
+
+
+
+
