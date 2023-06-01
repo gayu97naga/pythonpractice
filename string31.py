0f796d5a5f4c729c1cf7ca007d484ff50f69df3e
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-06-01 13:34:48 +0530

    Add files via upload

---
+++ string31.py
@@ -0,0 +1,68 @@
+##"Hello Bob!".
+
+name=(input("enter the name"))
+print("Hello Bob"+"!")
+
+## 'HiByeByeHi
+
+a=(input("enter a:"))
+b=(input("enter b:"))
+print(a+b+b+a)
+
+##<i>Yay</i>
+
+a=(input("enter a:"))
+b=(input("enter b:"))
+print("<"+a+">"+b+"<"+"/"+a+">")
+
+##<<Yay>>
+
+a=(input("enter a:"))
+b=(input("enter b:"))
+print(a[0:2]+b+a[2:4])
+
+##lololo
+a=(input("enter a:"))
+print(a[-2:]+a[-2:]+a[-2:])
+
+## hello first word:
+a=(input("enter a:"))
+print(a[0:2])
+
+
+
+##'HelloThere'=length
+
+a=(input("enter a:"))
+b=len(a)//2
+print(a[0:b])
+
+
+
+##coding:without_end
+
+a=(input("enter a:"))
+print(a[1:-1])
+
+
+## hiHellohi
+
+a=(input("enter a:"))
+b=(input("enter b:"))
+print(a+b+a)
+
+
+##hellothere:
+
+a=(input("enter a:"))
+b=(input("enter b:"))
+print(a[1:]+b[1:])
+
+
+##hello rotateleft:
+
+a=(input("enter a:"))
+print(a[2:]+a[0:2])
+
+
+
