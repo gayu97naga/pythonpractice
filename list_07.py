8458750b13aa984b23224bff838fa52ce7ad94e1
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-06-07 12:34:05 +0530

    Add files via upload

---
+++ list_07.py
@@ -0,0 +1,47 @@
+#list is first element grater than and last elements print true:
+
+a=[4,3,2,1]
+if(a,4>0):
+    print("True")
+else:
+    print("False")
+
+
+a=[1,2,3,5]
+if(len(a)>1):
+    if(a[0]>a[-1]):
+        print("True")
+    else:
+        print("False")
+else:
+    print("your list must be grater than 1")
+
+#common_end:
+a=[1,2,3]
+b=[5,3]
+if(len(a)>1):
+    if(a[-1]==b[1]):
+        print("True")
+    else:
+        print("False")
+else:
+    print("your list must be grater than 1")
+
+#same first element last:
+    
+a=[1,2,3,1]
+if(len(a)>1):
+    if(a[0]==b[1]):
+        print("True")
+    else:
+        print("False")
+else:
+    print("your list must be grater than 1")
+
+###sum of elements list:
+##a=[1,2,3,4,5]
+##sum=0
+
+
+
+
