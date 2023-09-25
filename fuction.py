5752c25cbf9ae6936561af86c2d566a1a3e0f3f1
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-09-25 16:26:12 +0530

    Add files via upload

---
+++ fuction.py
@@ -0,0 +1,42 @@
+#user defined fuctions:
+
+##def add_numbers(x,y):
+##    sum=x+y
+##    return sum
+##num1=5
+##num2=10
+##print("the sum is",add_numbers(num1,num2))
+
+
+#functions of odd or even:
+
+##def evenOdd(x):
+##    if(x%2==0):
+##        print("even")
+##    else:
+##        print("odd")
+##evenOdd(8)
+##evenOdd(9)
+
+
+#aruguments functions:
+
+##def myFunction(food):
+##    for x in food:
+##        print(x)
+##fruits=["apple","kiwi","cherry","banana"]
+##myFunction(fruits)
+
+
+#area and cicumference of a circle:
+
+##pi=3.14
+##r=int(input("enter the radius of a circle:"))
+##area=3.14*r*r
+##circumference=2*3.14*r
+##print("area of a circle",area)
+##print("circumference of a circle:2f",circumference)
+
+
+
+
