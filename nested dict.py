11332b5ff18963710306e86731fd692aec082adb
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-10-05 18:04:37 +0530

    Add files via upload

--- nested dict.py
+++ nested dict.py
@@ -35,33 +35,47 @@
 ##print(choc)
                 
 
-
-choc={"diarymilk":[5,10,20],"kitkat":[5,10,15],"lolipop":[5,10,15]}
-print(choc.keys())
-
 #user input:
-
-
-choc={}
-length=int(input("enter the length of dictionary"))
-for i in range (length):
-    keysvalue=input("enter the key value")
-    choc[keysvalue]={}
-    for i in range (length1):
-        if(i%2==0):
-            keysvalue2=input("enter the keysvalue2")
-            length1=int(input("enter the length1"))
-                for i in range (length2):
-                    keysvalue1=input("enter the key value1")
-                    choc[keysvalue][keysvalue1]={}
-
-
-
-##dict1={"diarymilk":[5,10,25],"kitkat":[5,10,15],"lolipop":[5,10,15]}
-##print(dict1)
+##chocolatelist={"diarymilk":[5,10,20],"kitkat":[5,10,15],"lolipop":[5,10,15]}
+##
 ##length=int(input("enter the length of dictionary"))
-##print("welcome to the shop")
-##for dict1 in range(length):
-##    keysvalues1=input("what do want choclate")
-##    dict1[keysvalues]=input("yes is cholate is avaiable")
-##    print("enter the quantity")
+##chocolatename=input("enter the chocolate name")
+##if(chocolatename in chocolatelist):
+##    customerchioce=int(input("enter the chocolate price"))
+##    pricelist=chocolatelist.get(chocolatename)
+##    if customerchioce in pricelist:
+##        noofchocolate=int(input("enter the no of chocolate do you want"))
+##        print("price",noofchocolate*customerchioce)
+##    else:
+##        print("chocolate is not available in that price")
+##else:
+##    print("not available")
+##
+##print(chocolatelist)
+
+
+
+
+#fromkeys:
+p=("p1","p2","p3","p4")
+q=5
+myDict=dict.fromkeys(p,q)
+print(myDict)
+
+#remove:
+programLanguages={1:"python",2:"java",3:"c++",4:"php",5:"sql"}
+if 3 in (programLanguages):
+    programLanguages[5]
+    print(programLanguages)
+
+#pop:
+
+programLanguages={1:"python",2:"java",3:"c++",4:"php",5:"sql"}
+print(programLanguages.pop(4))
+print(programLanguages)
+
+
+
+
+
+
