4ace460c00081256c79436b0064a00c586046001
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-10-20 17:56:21 +0530

    Add files via upload

---
+++ filehandling.py
@@ -0,0 +1,47 @@
+##fopen=open("demo.txt","r")
+##print(fopen.read())
+##print(fopen.read(5))
+##print(fopen.readline())
+##print(fopen.readline())
+##
+##
+##
+###write
+##
+##fopen=open("demo.txt","w")
+##fopen.write("welcome to gayathri")
+##print("data writed successfully")
+##fopen.close()
+##
+###append
+##
+##fopen=open("demo.txt","a")
+##fopen.write("ocean acadamey")
+##print("data writed successfully")
+##fopen.close()
+##
+##fopen=open("demo1.txt","x")
+##print("file created successfully")
+##
+##fopen=open("demo1.txt","w")
+##f.write("thankyou")
+##print("data writed successfully")
+##f.close()
+##
+##import os
+##os.remove("demo1.txt")
+##os.rmdir("ocean")
+
+##f=open("text.txt","r+")
+##print("before changing the index value",f.tell())
+##f.read()
+##f.write("python")
+##f.seek(0)
+##print(f.read())
+##print("after read operation",f.tell())
+##f.close()
+
+
+
+
+
