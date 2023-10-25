dcae38383b58fb543e20cfd1461f9adb38c62ae8
gayu97naga <132122806+gayu97naga@users.noreply.github.com>
2023-10-25 17:15:42 +0530

    Update exception handling.py

--- exception handling.py
+++ exception handling.py
@@ -54,10 +54,63 @@ finally:
 
 
 
+try:
+    a=int(input("enter a number1"))
+    b=int(input("enter a number2"))
+    print(a*b)
+except:
+    print("error")
+else:
+    print("no error")
+finally:
+    print("finally")
+print("program completed")
+
+#Exception Handling Using try,except
+
+try:
+    numerator = 10
+    denominator = 0
+
+    result = numerator/denominator
+
+    print(result)
+except:
+    print("Error: Denominator cannot be 0.")
+
+#Catching Specific Exceptions in Python
+
+try: 
+    even_numbers = [2,4,6,8]
+    print(even_numbers[5])
+
+except ZeroDivisionError:
+    print("Denominator cannot be 0.")    
+except IndexError:
+    print("Index Out of Bound.")
 
 
+#Python try with else clause
+try:
+    num = int(input("Enter a number: "))
+    assert num % 2 == 0
+except:
+    print("Not an even number!")
+else:
+    reciprocal = 1/num
+    print(reciprocal)
 
+#Python try,finally
 
+try:
+    numerator = 10
+    denominator = 0
 
+    result = numerator/denominator
 
-
+    print(result)
+except:
+    print("Denominator cannot be 0.")
+    
+finally:
+    print("This is finally block.")
